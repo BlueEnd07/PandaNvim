558aace7bc2ec8c0be5a1e7b2555ea492cbfaeb0
BlueEnd07 <lokeshrawat5050@gmail.com>
2024-01-22 17:45:50 +0530

    add none ls

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return{
-   'nvim-telescope/telescope.nvim', tag = '0.1.5',
+  { 
+  'nvim-telescope/telescope.nvim', tag = '0.1.5',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin=require("telescope.builtin")
@@ -7,4 +8,19 @@ return{
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
   end
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config=function()
+      require("telescope").setup {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      }
+    require("telescope").load_extension("ui-select")
+    end
+  }
 }
