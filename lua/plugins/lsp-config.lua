2dd483aa3ac2fa1544bbf612d0169b2fb68e3cf5
BlueEnd07 <lokeshrawat5050@gmail.com>
2024-01-22 19:51:22 +0530

    add auto completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,7 +15,9 @@ return{
   },
   {
     "neovim/nvim-lspconfig",
+    lazy=false,
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig=require('lspconfig')
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
